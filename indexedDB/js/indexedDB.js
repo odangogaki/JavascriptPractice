d459268b71aac6169cf752b380292bb67a576571
odangogaki <h.odagaki825@outlook.jp>
2018-07-08 23:30:53 +0900

    indexedDB
    indexedDBの練習

---
+++ indexedDB/js/indexedDB.js
@@ -0,0 +1,85 @@
+var db;
+
+// DBを開くメソッド（DBの新規作成、upgradeも含む）
+function openDB(DBName, DBVersion, DBStoreName){
+  // requestにはIDBOpenDBRequestオブジェクトのインスタンスが格納される
+  // IDBOpenDBRequestオブジェクトは結果（成功）またはイベントとして扱うエラー値を伴うオブジェクト
+  //open()関数の第一引数はDB名、第二引数はバージョン(long型)
+  //DBがない場合新規のDBが作成される
+  var request = window.indexedDB.open(DBName, DBVersion);
+  // DBが存在しない、もしくはversionが古い場合、新規作成アップグレード
+  request.onupgradeneeded = function(event){
+    console.log('DBUPGRADE');
+    // オブジェクトストアを作成
+    var store = event.currentTarget.result.createObjectStore(
+      DBStoreName,
+      {keyPath: "id"}
+    );
+    // このイベントを正常に抜けたらonsuccessハンドラが実行される
+  };
+  // requestが成功したとき
+  request.onsuccess = function(event){
+    //成功時の処理、request.resultに対して行うこと
+    // 成功eventがrequestをtargetとして発生する
+    //変数dbに成功時のeventのtrget(つまりrequest)の結果を格納
+    // event.target==request, request.result==IDBDatabaseのインスタンス
+    db = this.result;
+    console.log("DBOPEN!");
+  };
+  //requestが失敗したとき
+  request.onerror = function(event){
+    //error時の処理、request.errorCodeに対して行うこと
+    // errorEventがrequestをtargetとして発生する
+    console.error("openDB: ", event.target.errorCode);
+  };
+}
+
+function getObjectStore(storeName, mode){
+  var transaction = db.transaction(storeName, mode);
+  return transaction.objectStore(storeName);
+}
+
+function getData(store, key){
+  // var store = getObjectStore(storeName, 'readwrite');
+  var request = store.get(key);
+  request.onsuccess = function(event){
+    var value = event.target.result;
+    return value;
+  }
+}
+
+// 全データを読み出す
+function getAllData(storeName){
+  var dataArray = [];
+  var transaction = db.transaction(storeName, 'readwrite');
+  var store = transaction.objectStore(storeName);
+  store.openCursor().onsuccess = function(event){
+    var cursor = event.target.result;
+    if(cursor){
+      dataArray.push(cursor.value);
+      console.log(dataArray);
+      cursor.continue();
+    }else{
+      alert('allDataGetDone');
+      console.log(dataArray);
+      return dataArray;
+    }
+  }
+}
+
+// function addData(store, dictionary)
+
+function putData(storeName, dictionary){
+  var store = getObjectStore(storeName, 'readwrite');
+  var request = store.put(dictionary);
+  request.onsuccess = function(){
+    console.log('putOK!!');
+  }
+}
+
+// ファイル名取得用メソッド
+function getFileName(){
+  var url = location.href;
+  var fileName = url.split('/').pop();
+  return fileName;
+}
---
+++ indexedDB/js/testPage1.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+  // 登録を押下した回数をカウント
+  var registerCount = 0;
+  var outputText = '';
+  $('#register_button').on({
+    'click' : function(){
+      var inputText = $('#input_form').val();
+      var key = getFileName() + registerCount;
+      var dictionary = {id: key, value: inputText};
+      openDB("testDB", 1, "testStore");
+      setTimeout(function(){
+        // var storeObject = getObjectStore("testStore", 'readwrite');
+        putData("testStore", dictionary);
+        // var outputData = getData(storeObject, key);
+        // outputText += outputData + '<br/>'
+        // $('#output_area').html(outputText);
+        registerCount += 1;
+      }, 1000);
+    }
+  });
+
+  // なぜか関数化したらうまくとりだせない・・・
+  //getAllDataメソッドがなぜか2回はしり、1回目にはopenCursor()がsuccessにならず、2回目にsucccessになるため
+  // 時間遅延でoutputしようとしたがreturnでの値が返ってこない
+  // なぜ？？20180709
+  $('#output_button').on({
+    'click' : function(){
+      // var storeObject = getObjectStore("testStore", 'readwrite');
+      var outputData = getAllData("testStore");
+      setTimeout(function(){
+        $('#output_area').html(outputData);
+      }, 1000);
+    }
+  })
+
+
+})
